0b32980f1f8603395a39a9928b6c9ffb403f76ad
lilauzzie <lilaussie24@gmail.com>
2014-12-30 11:06:21 +1000

    Created basic mod class

---
+++ src/main/java/com/lilauzzie/weatherplus/WeatherPlus.java
@@ -0,0 +1,34 @@
+package com.lilauzzie.weatherplus;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by lilauzzie on 30/12/2014.
+ * This file is used in the Weather Plus project.
+ */
+
+@Mod(modid = "WeatherPlus", name = "Weather Plus", version = "1.7.10-1.0")
+public class WeatherPlus {
+
+    @Mod.Instance("WeatherPlus")
+    public static WeatherPlus instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+}
