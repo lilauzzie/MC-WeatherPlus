8ee1dd869b624f767ed318fb21222416c41e71b0
lilauzzie <lilaussie24@gmail.com>
2014-12-30 12:25:57 +1000

    Added classes.

---
+++ src/main/java/com/lilauzzie/weatherplus/proxy/IProxy.java
@@ -0,0 +1,10 @@
+package com.lilauzzie.weatherplus.proxy;
+
+/**
+ * Created by lilauzzie on 30/12/2014.
+ * This file is used in the MC-WeatherPlus project.
+ */
+
+public interface IProxy {
+
+}
---
+++ src/main/java/com/lilauzzie/weatherplus/reference/Reference.java
@@ -0,0 +1,14 @@
+package com.lilauzzie.weatherplus.reference;
+
+/**
+ * Created by lilauzzie on 30/12/2014.
+ * This file is used in the MC-WeatherPlus project.
+ */
+
+public class Reference {
+    public static final String MOD_ID = "WeatherPlus";
+    public static final String MOD_NAME = "Weather Plus";
+    public static final String MOD_VERSION = "1.7.10-1.0";
+    public static final String CLIENT_PROXY_CLASS = "com.lilauzzie.weatherplus.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.lilauzzie.weatherplus.proxy.ServerProxy";
+}
